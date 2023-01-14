88dabde53ce80680a77ec806f8a8138cf8a0763b
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-14 01:23:49 +0100

    logger

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import config from "config";
+import log from "./logger";
 
 async function connect(){
     const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri)
-    console.log("Database connected successfully 🚀");
+    log.info("Database connected successfully 🚀");
     
   } catch (error) {
-    console.log("could not connect to database");
+    log.error("could not connect to database");
     process.exit(1);
     
   }

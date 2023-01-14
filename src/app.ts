89ead5fe5f477339e8a747660c43d98ba33250e4
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-14 07:18:08 +0100

    api healthcheck

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import  config  from 'config';
 import connect from './utils/database';
 import log from "./utils/logger";
+import routes from './routes/api';
+
 
 const port = config.get<number>("port")
 
@@ -11,4 +13,6 @@ app.listen(port, async ()=>{
     log.info(`App is running on http://localhost:${port}`);
     
     await connect()
+
+    routes(app)
 })
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,12 @@
-import {Express} from 'express';
+import {Express, Request, Response} from 'express';
 
-const routes = ()=>{
-
+const routes = (app: Express)=>{
+  app.get('/healthcheck', (req:Request, res: Response)=>{
+    res.json({
+        status: true,
+        message: "Wellcome to your project manager!!!"
+    })
+  })
 }
 
 export default routes;

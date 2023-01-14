e788b94bc6af0c012a680701baeca0467c8d37c0
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-14 01:21:38 +0100

    logger function

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import  config  from 'config';
 import connect from './utils/database';
+import log from "./utils/logger";
 
 const port = config.get<number>("port")
 
 const app = express()
 
 app.listen(port, async ()=>{
-    console.log(`App is running on ${port}`);
+    log.info(`App is running on http://localhost:${port}`);
     
     await connect()
 })

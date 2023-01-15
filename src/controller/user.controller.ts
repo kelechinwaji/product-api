42c6dfcdd33d2c4a6d9e9d0d14a3fb020d1a16fc
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-15 23:53:27 +0100

    user service instance

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,11 @@
 import {Request, Response} from "express"
+import { createUser } from "../service/user.service";
 import log from "../utils/logger"
 
-export const CreateUserHandler = (req:Request, res:Response)=>{
+export const CreateUserHandler = async (req:Request, res:Response)=>{
  try {
-    
+    const result = await createUser(req.body);
+    return {status: true, message: "User created successfully", data: result};
  } catch (error) {
     log.error(error)
     return res.json({

815906e3ceb181781b1d4eba10ff293b916494fc
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-15 23:13:47 +0100

    bcrypt compare password

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,6 +8,7 @@ export interface UserDocument extends mongoose.Document{
     password: string,
     createdAt: Date,
     updatedAt: Date,
+    comparePassword: (candidatePassword: string): Promise<boolean>
 }
 
 const userSchema  = new mongoose.Schema({
@@ -32,6 +33,13 @@ userSchema.pre("save", async function (next){
 
   return next();
 });
+
+// Compare password
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean>{
+   const user = this as UserDocument;
+
+   return bcrypt.compare(candidatePassword, user.password).catch((error) => false);
+}
 
 const UserModel = mongoose.model("User", userSchema);
 

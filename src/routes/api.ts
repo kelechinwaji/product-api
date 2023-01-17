8b3cf88715aabe3de17e629b787386b7755db17a
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-17 07:32:41 +0100

    create user route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,5 @@
 import {Express, Request, Response} from 'express';
+import { CreateUserHandler } from '../controller/user.controller';
 
 const routes = (app: Express)=>{
   app.get('/healthcheck', (req:Request, res: Response)=>{
@@ -7,6 +8,8 @@ const routes = (app: Express)=>{
         message: "Wellcome to your project manager!!!"
     })
   })
+
+  app.post("/api/users", CreateUserHandler)
 }
 
 export default routes;

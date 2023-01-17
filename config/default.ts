a21eeb5493d69329a654d427c63bc0975c135aae
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-18 00:29:59 +0100

    jwt keys

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,19 @@
 export default {
     port: 1337,
     dbUri: "mongodb://localhost:27017/product-api",
-    salt: 15
+    salt: 15,
+    publicKey: `-----BEGIN PUBLIC KEY-----
+    MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMxYX6783v1v7j2ex18A2etTV0NGTx/8
+    u8j3hGhexB+Af8n/a67vJ10Z8g+mS+iQug8iE/7dk/vnbjp1Wju4sL8CAwEAAQ==
+    -----END PUBLIC KEY-----`,
+    privateKey: `-----BEGIN PRIVATE KEY-----
+    MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEAzFhfrvze/W/uPZ7H
+    XwDZ61NXQ0ZPH/y7yPeEaF7EH4B/yf9rru8nXRnyD6ZL6JC6DyIT/t2T++duOnVa
+    O7iwvwIDAQABAkEAsqMfIo6VMeFD2EKR8A+CciKtSnaQfVonn4wiCEuwEAhpsewM
+    2kcdQrVoIXHBliR9zElFYLBbDw0xA9GSFqeGAQIhAOmlz7qFPuCFCsJrBDwReL0f
+    GmNifP1xFuwO3wHi2qR/AiEA3+Tsud/oVGJDE99/20sCzvIt0v2MBXwahY/KpuGy
+    08ECIQDVNl+KyR8oyZ+Y7QmPOy5f/usYuH5w1Oti/OsakIaPRwIhAKw2YGNkN0bY
+    wfGCQs0BOGrHWrnkbhQ17gx3mo+cILnBAiAUr7cNN/EqR8pyCHqONDbnISPeipER
+    uhJ4vIxnZKuc6A==
+    -----END PRIVATE KEY-----`
 }

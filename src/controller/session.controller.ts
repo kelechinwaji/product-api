4ec48740e9f9f7d973eeb85443c09811e8a0daa4
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-18 00:04:32 +0100

    validate user's password

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -4,7 +4,14 @@ import { validatePassword } from "../service/user.service";
 export const createUserSessionHandler = async (req:Request, res:Response)=>{
 
     //Validate the user's password
-    const user = await validatePassword(req.body)
+    const user = await validatePassword(req.body);
+
+    if (!user){
+        res.json({
+            status: false,
+            message: "Invalid email or password"
+        })
+    }
 
     //Create a session
 

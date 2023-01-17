7aff0c22813e2c2c2e2bb8d85a912a650e2f5d90
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-17 15:54:07 +0100

    session controller

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,12 @@
+import {Request, Response} from "express";
+
+export const createUserSessionHandler = (req:Request, res:Response)=>{
+
+    //Validate the user's password
+
+    //Create a session
+
+    //Create an access token
+
+    //Create a refresh token
+}

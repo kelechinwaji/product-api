27d103c630a3e55bf0943fe4bb2b086de3607887
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-24 18:43:54 +0100

    session document

--- src/models/sessions.model.ts
+++ src/models/sessions.model.ts
@@ -19,6 +19,6 @@ const sessionSchema  = new mongoose.Schema({
 });
 
 
-const SessionModel = mongoose.model("Session", sessionSchema);
+const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
 
 export default SessionModel;

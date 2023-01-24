173e1b23a8b558128a1cf794eeb8da3b1fde22c0
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-24 19:02:44 +0100

    Reissue expired access token

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,6 +1,10 @@
+import config from "config";
+import { get } from "lodash";
 import { FilterQuery, UpdateQuery } from "mongoose";
 import SessionModel, { SessionDocument } from "../models/sessions.model"
 import UserModel from "../models/user.model";
+import { signJwt, verifyJwt } from "../utils/jwt.utils";
+import { findUser } from "./user.service";
 
 export const createSession = async (userId: string, userAgent: string)=>{
     const session = await SessionModel.create({user: userId, userAgent});
@@ -16,4 +20,24 @@ export const findSessions = (query: FilterQuery<SessionDocument>)=>{
 
 export const updateSession = (query: FilterQuery<SessionDocument>, update: UpdateQuery<SessionDocument>)=>{
   return SessionModel.updateOne(query, update);
+}
+
+export const reIssueAccessToken = async({refreshToken,}:{ refreshToken: string})=>{
+  const {decoded} = verifyJwt(refreshToken);
+
+  if(!decoded || !get(decoded, "_id")) return false;
+
+  const session = await SessionModel.findById(get(decoded, "_id"));
+
+  if (!session || !session.valid) return false;
+
+  const user = await findUser({_id: session.user});
+
+  if(!user) return false;
+
+  const accessToken = signJwt(
+    {...user, session: session._id,}, 
+    {expiresIn: config.get<string>("accessTokenTtl") });
+
+  return accessToken;
 }

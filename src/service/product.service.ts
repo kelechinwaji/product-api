ef9af3fcd1e947fc94db0938172eabcbb6c88c13
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-25 04:55:26 +0100

    find product query service

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,6 +1,10 @@
-import { DocumentDefinition } from "mongoose";
+import { DocumentDefinition, FilterQuery, QueryOptions } from "mongoose";
 import ProductModel, { ProductDocument } from "../models/product.model";
 
 export const createProduct = async(input:DocumentDefinition<Omit<ProductDocument,"createdAT" | "updatedAT">>)=>{
  return ProductModel.create(input);
+}
+
+export const findProduct = (query: FilterQuery<ProductDocument>, options: QueryOptions = {lean: true})=>{
+ return ProductModel.findOne(query, {}, options);
 }

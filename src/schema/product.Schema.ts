7d1f54e94321f7dc49ea99a1fa934cd0cc1ffcbd
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-25 05:18:28 +0100

    product payload schema

---
+++ src/schema/product.Schema.ts
@@ -0,0 +1,18 @@
+import { number, object, string } from "zod";
+
+const payload = {
+    body: object({
+        title: string({
+            required_error: "Title is required"
+        }),
+        description: string({
+            required_error: "Description is required"
+        }).min(120, "Description should be at least 120 characters long"),
+        price: number({
+            required_error: "Price is required"
+        }),
+        image: string({
+            required_error: "Image is required"
+        }),
+    })
+}

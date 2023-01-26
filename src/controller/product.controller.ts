885eace89e96c3b744046ed66aba3da958df9657
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-26 08:15:17 +0100

    update product controller

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
-import { CreateProductInput } from "../schema/product.Schema";
-import { createProduct } from "../service/product.service";
+import { CreateProductInput, UpdateProductInput } from "../schema/product.Schema";
+import { createProduct, findAndUpdateProduct, findProduct } from "../service/product.service";
 
 export const createProductHandler =async (req:Request<{}, {}, CreateProductInput["body"]>, res:Response)=>{
  try {
@@ -18,4 +18,35 @@ export const createProductHandler =async (req:Request<{}, {}, CreateProductInput
         message: error
     })
  }
+}
+
+export const getProductHandler =async (req:Request<UpdateProductInput["params"]>, res:Response)=>{
+    try {
+        const userId = res.locals.user._id;
+        const productId = req.params.productId;
+        const update = req.body;
+        
+        const product = await findProduct({productId});
+     
+        if(!product){
+         return res.sendStatus(404)
+        }
+     
+        if(product.user != userId){
+          return res.sendStatus(403)
+        }
+
+        const result = await findAndUpdateProduct({productId}, update, {new: true})
+         return res.json({
+            status: true,
+            message: "Updated successfull",
+            data: result
+         })
+    } catch (error) {
+        return res.json({
+            status: false,
+            message: error
+        })
+    }
+  
 }

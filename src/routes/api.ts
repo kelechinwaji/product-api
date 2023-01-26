74494824ca8b888efc51adde4177b568df7681ed
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-01-26 22:25:19 +0100

    product routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,6 +7,7 @@ import { createSessionSchema } from '../schema/session.schema';
 import { createUserSchema } from '../schema/user.schema';
 
 const routes = (app: Express)=>{
+  //Homepage
   app.get('/healthcheck', (req:Request, res: Response)=>{
     res.json({
         status: true,
@@ -14,10 +15,14 @@ const routes = (app: Express)=>{
     })
   })
 
+  //User routes
   app.post("/api/users",validate(createUserSchema), CreateUserHandler);
   app.post("/api/sessions",validate(createSessionSchema), createUserSessionHandler);
   app.get("/api/sessions", requireUser, getUserSessionsHandler)
   app.delete("/api/sessions", requireUser, deleteUserSessionsHandler)
+
+  //Product routes
+   app.post("/api/products", [requireUser, validate])
 }
 
 export default routes; 

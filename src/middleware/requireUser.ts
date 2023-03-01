08957d8abe40748c9af4067a98135ac0643ab53f
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-01 20:52:53 +0100

    [chore]:clean up

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -4,8 +4,6 @@ import {Request, Response, NextFunction } from "express";
 const requireUser = (req:Request, res:Response, next:NextFunction)=>{
  const user = res.locals.user;
 
- console.log(user, "require");
- 
 
  if(!user){
     return res.sendStatus(403);
